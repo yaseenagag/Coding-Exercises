e8842612a58c1fe0cffeee9fb0d13040e7853356
yaseenagag <yaseen.uc@gmail.com>
2017-02-09 17:01:58 -0800

    refactor for optimoized performance

--- src/Specifications/fibonacci.js
+++ src/Specifications/fibonacci.js
@@ -1,32 +1,17 @@
-// function fibonacci(input, cb) {
-//   let result = [];
-//
-//   if( input <= 1 ) return 1;
-//
-//   let newValue = fibonacci(input - 1, cb) + fibonacci(input - 2, cb)
-//   cb(newValue)
-//   return(newValue)
-//
-// }
-//
-//
-// console.log(fibonacci(6, v => {
-//   console.log(v)
-// }))
+function fibonacci(input) {
+  let current = 0,
+    pervious = 1,
+    fibonacci = 1,
+    result = []
 
-function fibonacci(n, cb) {
-    if(n>=1){
-      cb(1)
-    }
+    for(var index = 2; index <= input+1; index++) {
+        fibonacci = current + previous
+        current = previous
+        previous = fibonacci
+        result.push(previous)
 
-    var a = 0, b = 1, f = 1;
-    for(var i = 2; i <= n+1; i++) {
-        f = a + b;
-        a = b;
-        b = f;
-        cb(f)
     }
-    return f;
+    return fibonacci;
 };
 
-fibonacci(6, console.log)
+console.log(fibonacci(6))
